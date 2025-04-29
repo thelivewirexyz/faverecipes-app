6f3dbfb5f1caf2935d6120a58ad20992db686eaa
thelivewirexyz <thelivewirexyz@gmail.com>
2025-04-29 16:03:31 +0100

    udpates

--- app.js
+++ app.js
@@ -132,16 +132,23 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Optional: adds a smooth scrolling animation
+  });
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
+  document.body.style.overflow = "auto";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    document.body.style.overflow = "auto";
   }
 };
